a25b9557797898a762bd7ea3cdf87971bd946c14
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-05 22:13:45 +0300

    shared: add response dto and no content classes

---
+++ Shared/ECourse.Shared/Dtos/NoContent.cs
@@ -0,0 +1,6 @@
+namespace ECourse.Shared.Dtos
+{
+    public class NoContent
+    {
+    }
+}
--- Shared/ECourse.Shared/Dtos/ResponseDto.cs
+++ Shared/ECourse.Shared/Dtos/ResponseDto.cs
@@ -1,7 +1,40 @@
 namespace ECourse.Shared.Dtos
 {
-    class ResponseDto
+    public class ResponseDto<T>
     {
+        public T Data { get; private set; }
 
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+
+        public List<string> Errors { get; private set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode = 200)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode };
+        }
+
+        public static ResponseDto<T> Success(int statusCode = 200)
+        {
+            return new ResponseDto<T> { Data = default(T), StatusCode = statusCode };
+        }
+
+        public static ResponseDto<T> Fail(List<string> errors, int statusCode = 400)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode
+            };
+        }
+
+        public static ResponseDto<T> Fail(string error, int statusCode = 400)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = new List<string> { error },
+                StatusCode = statusCode
+            };
+        }
     }
 }
