76404c2af52dbcd62422666897b46cdfeeece95b
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-05 22:27:07 +0300

    catalog: add mapping folder and general mapping class

---
+++ Services/Catalog/ECourse.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using ECourse.Services.Catalog.Dtos;
+using ECourse.Services.Catalog.Models;
+
+namespace ECourse.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        }
+    }
+}
