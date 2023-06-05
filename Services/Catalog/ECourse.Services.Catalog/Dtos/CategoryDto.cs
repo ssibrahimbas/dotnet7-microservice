0b5447ba71234a83a82624b279c7f91338b76c5d
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-05 22:19:32 +0300

    catalog: add course, feature and category dtos

---
+++ Services/Catalog/ECourse.Services.Catalog/Dtos/CategoryDto.cs
@@ -0,0 +1,8 @@
+namespace ECourse.Services.Catalog.Dtos
+{
+    public class CategoryDto
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ Services/Catalog/ECourse.Services.Catalog/Dtos/FeatureDto.cs
@@ -0,0 +1,7 @@
+namespace ECourse.Services.Catalog.Dtos
+{
+    public class FeatureDto
+    {
+        public int Duration { get; set; }
+    }
+}
